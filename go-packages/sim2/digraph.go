b680640c8274e79001b1d79b4ecb77985ecd0776
nathik azad <nathikazad@gmail.com>
2018-05-14 18:30:16 -0700

    Pick up and drop off works for the most part

--- go-packages/sim2/digraph.go
+++ go-packages/sim2/digraph.go
@@ -17,7 +17,7 @@ import (
 type Vertex struct {
   ID uint
   Pos Coords
-  AdjEdges []Edge
+  AdjEdges []*Edge
   intersection *Intersection
   directionFromIntersection Direction
 }
@@ -125,7 +125,7 @@ func GetDigraphFromFile(fname string) (d *Digraph) {
       edge.Start = vert
       edge.End = vertNext
       d.Edges[edge.ID] = edge
-      vert.AdjEdges = append(vert.AdjEdges, *edge)
+      vert.AdjEdges = append(vert.AdjEdges, edge)
     }
 
     // Set starting edge weight based on distance
@@ -171,31 +171,135 @@ func splitLine(line string, separator string, length int) (numbers []float64) {
 }
 
 // ShortestPath - solve for the shortest deighted directional path from start to end vertex.
-func (g Digraph) shortestPath(startVertID, endVertID uint) (edges []Edge, dist float64) {
-	s1 := rand.NewSource(time.Now().UnixNano())
-	r1 := rand.New(s1)
-	adjEdges := g.Vertices[startVertID].AdjEdges
-	edge := adjEdges[uint(r1.Int() % len(adjEdges))]
-	edges = append(edges, edge)
+//   If not path can be found, return an empty slice and infinite distance.
+//   Negative edge weights are not permitted.
+func (g *Digraph) shortestPath(startVertID, endVertID uint) (edges []Edge, dist float64) {
+	edgeIDs := make([]uint, 0)
+	dist = math.Inf(1)
 
-	adjEdges = edge.End.AdjEdges
-	edge = adjEdges[uint(r1.Int() % len(adjEdges))]
-	edges = append(edges, edge)
+	// NOTE: this shortest path implementation follows ShortestPath's algorithm
 
-	adjEdges = edge.End.AdjEdges
-	edge = adjEdges[uint(r1.Int() % len(adjEdges))]
-	edges = append(edges, edge)
+	// Receiver validity check
+	if g.Vertices == nil || g.Edges == nil {
+		log.Println("err: ShortestPath - digraph not initialized")
+		return
+	}
 
-	adjEdges = edge.End.AdjEdges
-	edge = adjEdges[uint(r1.Int() % len(adjEdges))]
-	edges = append(edges, edge)
+	// Bounds check; ensure start and end vertices are in the graph
+	_, ok0 := g.Vertices[startVertID]
+	_, ok1 := g.Vertices[endVertID]
+	if !ok0 || !ok1 {
+		log.Println("err: ShortestPath - invalid start or end vertex")
+		return
+	}
 
-	adjEdges = edge.End.AdjEdges
-	edge = adjEdges[uint(r1.Int() % len(adjEdges))]
-	edges = append(edges, edge)
+	// Mark all vertices as 'unvisited'
+	unvisited := make(map[uint]bool)  // Set of vertex IDs
+	for idx := range g.Vertices {
+		unvisited[idx] = true
+	}
+	//fmt.Println("unvisited:", unvisited)
 
-	// TODO: implement this in a concurrency-safe manner: do not modify underlying values!
-  return
+	// Define local struct type to map minimum distance to previous vertex
+	type minDist struct {
+		dist float64  // Minimum distance to start from this vertex through previous vertex
+		prev uint  // ID of previous vertex for this distance
+	}
+
+	// Assign each vertex a tentative distance value from start: +Inf by default
+	distances := make(map[uint]minDist)
+	for idx := range g.Vertices {
+		distances[idx] = minDist{math.Inf(1), 0}
+	}
+	distances[startVertID] = minDist{float64(0), 0}  // starting vertex has no distance to itself
+
+	// Iterate over all vertices, starting at the query vertex, until end vertex is found
+	currID := startVertID
+	for ; len(unvisited) != 0 ; {
+		// Create a local record of the shortest distance in this iteration (optimization)
+		shortest := minDist{math.Inf(1), 0}
+
+		// Consider all unvisited neighbors of the current vertex
+		for _, adjEdge := range g.Vertices[currID].AdjEdges {
+			neighborID := adjEdge.End.ID  // Identify neighbor vertex
+
+			// Only consider if unvisited
+			if _, ok := unvisited[neighborID]; ok {
+
+				// Determine distance to start vertex
+				localdist := distances[currID].dist + adjEdge.Weight
+				//fmt.Println("currID", currID, "neighborID", neighborID, "adjEdge", adjEdge)
+				//fmt.Println("localdist", localdist, "currdist", distances[currID].dist, "weight", adjEdge.Weight)
+
+				// If this distance is shorter than recorded distance, update
+				if localdist < distances[neighborID].dist {
+					distances[neighborID] = minDist{localdist, currID}
+				}
+
+				// If this is the shortest distance for the current vertex, note so
+				if localdist < shortest.dist {
+					shortest = minDist{localdist, neighborID}
+				}
+			}
+		}
+
+		// If identified shortest is valid, use it as next vertex to visit
+		var nextID uint
+		if !math.IsInf(shortest.dist, 1) {
+			nextID = uint(shortest.prev)
+		} else {  // Otherwise, search graph for next closest vertex
+			nextClosest := minDist{math.Inf(1), 0}  // Identify next closest vertex
+
+			// Walk the unvisited vertices to find the next closest vertex
+			for idx := range unvisited {
+				if distances[idx].dist < nextClosest.dist {
+					nextClosest = minDist{distances[idx].dist, idx}
+				}
+			}
+
+			// Assign next vertex as the closest unvisited vertex
+			nextID = uint(nextClosest.prev)
+		}
+
+		// Identify current vertex as 'visited' and move to next vertex
+		delete(unvisited, currID)
+		currID = nextID
+	}
+
+	//fmt.Println("DONE")
+
+	// Determine if a valid path was found
+	if !math.IsInf(distances[endVertID].dist, 1) {
+		inverse := make([]uint, 0)
+		dist = float64(0)
+
+		// Build the path backwards from the destination vertex
+		inverse = append(inverse, endVertID)
+		for curr := endVertID; curr != startVertID; curr = distances[curr].prev {
+			inverse = append(inverse, distances[curr].prev)
+			dist += distances[curr].dist
+			//fmt.Println("cd", distances[curr].dist)
+		}
+
+		// Invert the path back for the path from start
+
+		len := len(inverse)
+		for idx := len - 1; idx >= 0; idx-- {
+			edgeIDs = append(edgeIDs, inverse[idx])
+		}
+	}
+
+	for i :=0 ; i < len(edgeIDs)-1; i++ {
+		vertex := g.Vertices[edgeIDs[i]]
+		for _, adjEdge := range vertex.AdjEdges {
+			if edgeIDs[i+1] == adjEdge.End.ID {
+				edges = append(edges, *adjEdge)
+				break
+			}
+		}
+	}
+
+	return
 }
 
 // closestEdgeAndCoord For coords within world space, find  closest coords on an edge on world graph
